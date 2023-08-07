36e78b34bad38eea1520dba0a923f50991b975a0
NguyenQuocKhanh0 <khanh200175@gmail.com>
2023-08-07 23:03:42 +0700

    up

--- train.py
+++ train.py
@@ -107,8 +107,11 @@ def run(rank, n_gpus, hps):
 
   try:
     print("loading---------------")
-    _, _, _, epoch_str = utils.load_checkpoint("/kaggle/input/checkpointdg/G_8000.pth", net_g, optim_g)
-    _, _, _, epoch_str = utils.load_checkpoint("/kaggle/input/checkpointdg/D_8000.pth", net_d, optim_d)
+    # _, _, _, epoch_str = utils.load_checkpoint("/kaggle/input/checkpointdg/G_8000.pth", net_g, optim_g)
+    # _, _, _, epoch_str = utils.load_checkpoint("/kaggle/input/checkpointdg/D_8000.pth", net_d, optim_d)
+    _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g, optim_g)
+    _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d, optim_d)
+    global_step = (epoch_str - 1) * len(train_loader)
     global_step = (epoch_str - 1) * len(train_loader)
     print("____________",epoch_str)
   except:
@@ -231,10 +234,10 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
 
       if global_step % hps.train.eval_interval == 0:
         evaluate(hps, net_g, eval_loader, writer_eval)
-        utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
-        utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
-        old_g=os.path.join(hps.model_dir, "G_{}.pth".format(global_step-2000))
-        old_d=os.path.join(hps.model_dir, "D_{}.pth".format(global_step-2000))
+        utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, "/kaggle/working/checkpoint/G_{}.pth".format(global_step))
+        utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, "/kaggle/working/checkpoint/D_{}.pth".format(global_step))
+        old_g="/kaggle/working/checkpoint/G_{}.pth".format(global_step-2000))
+        old_d="/kaggle/working/checkpoint/G_{}.pth".format(global_step-2000))
         if os.path.exists(old_g):
           os.remove(old_g)
         if os.path.exists(old_d):
