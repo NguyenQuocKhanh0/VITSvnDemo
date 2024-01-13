1f91d2291b082712bcb16d7f749a8079db89cdd4
NguyenQuocKhanh0 <108500182+NguyenQuocKhanh0@users.noreply.github.com>
2024-01-13 15:50:44 +0700

    Update single_speaker.py

--- single_speaker.py
+++ single_speaker.py
@@ -55,13 +55,15 @@ if __name__ == '__main__':
 
     net_g, _, _, _ = utils.load_checkpoint(path_to_model, net_g, None)
     net_g.eval()
-    print(net_g)
+    # print(net_g)
 
     text = args.text
     stn_tst = get_text(text, hps)
     with torch.no_grad():
         x_tst = stn_tst.cpu().unsqueeze(0)
+        print(x_tst)
         x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cpu()
+        print(x_tst_lengths)
         audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
     ipd.display(ipd.Audio(audio, rate=hps.data.sampling_rate, normalize=False))
     print("successful")
